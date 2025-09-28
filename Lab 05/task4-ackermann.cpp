6fded85827a7801fa927088efea050deb3179957
Ismail Silat <ismailsilat7@gmail.com>
2025-09-28 19:14:32 +0500

    Ackermann

---
+++ Lab 05/task4-ackermann.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ackermann(int m, int n) {
+    if(m == 0) {
+        return n + 1;
+    }
+    if(m > 0 && n == 0) {
+        return ackermann(m - 1, 1);
+    }
+    if(m > 0 && n > 0) {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
+    return -1;
+}
+
+int main() {
+    cout << ackermann(3, 2) << endl;
+}
