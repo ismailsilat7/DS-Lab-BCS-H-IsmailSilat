66c001d62320b37a59c10d265fe55adcae906950
Ismail Silat <ismailsilat7@gmail.com>
2025-12-01 06:32:07 +0500

    Add rehashing

--- prac/separateChaining.cpp
+++ prac/separateChaining.cpp
@@ -18,8 +18,9 @@ class HashTable {
 public:
     Node** table;
     int c;
-
+    int numElements;
     HashTable(int c) : c(c) {
+        numElements = 0;
         table = new Node*[c];
         for(int i = 0; i < c; i++) {
             table[i] = nullptr;
@@ -30,12 +31,41 @@ public:
         return k % c;
     }
 
+    void rehash() {
+        Node** oldTable = table;
+        int oldC = c;
+        int size = 0;
+
+        c = c * 2 + 1;
+        table = new Node*[c];
+        for(int i=0; i<c; i++) table[i] = nullptr;
+
+        for(int i = 0; i < oldC; i++) {
+            Node* temp = oldTable[i];
+            while(temp) {
+                insert(temp->key, temp->data);
+                Node* p = temp;
+                temp = temp->next;
+                delete p;
+            }
+        }
+        delete[] oldTable;
+    }
+
+    float loadFactor() {
+        return (float)numElements/c;
+    }
+
     void insert(int key, int data = 0) {
+        if(loadFactor() > 0.7) {
+            rehash();
+        }
         int index = hash(key);
         Node* p = new Node(key, data);
 
         p->next = table[index];
         table[index] = p;
+        numElements++;
     }
 
     void deleteNode(int key) {
@@ -58,6 +88,7 @@ public:
                     prev->next = curr->next;
 
                 delete curr;
+                numElements--;
                 return;
             }
 
